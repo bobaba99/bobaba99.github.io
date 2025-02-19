fc9613ecff71a76cb0c770cc66e40c47d37c5ad6
Gavin <53244273+bobaba99@users.noreply.github.com>
2025-02-19 18:19:52 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
